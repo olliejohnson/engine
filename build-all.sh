05933bda62aeca5e5d03352f19bec42cc5575459
olliejohnson <oliver.joseph.johnson@gmail.com>
2025-08-27 12:07:28 -0400

    update build system

--- build-all.sh
+++ build-all.sh
@@ -1,13 +1,10 @@
-#!/bin/bash
-# Build script for rebuilding everything
-set echo on
-
 echo "Building everything..."
 
 
-pushd engine
-source build.sh
-popd
+# pushd engine
+# source build.sh
+# popd
+make -f Makefile.engine.linux.mak all
 
 ERRORLEVEL=$?
 if [ $ERRORLEVEL -ne 0 ]
@@ -15,13 +12,11 @@ then
 echo "Error:"$ERRORLEVEL && exit
 fi
 
-pushd testbed
-source build.sh
-popd
+# pushd testbed
+# source build.sh
+# popd
+
+make -f Makefile.testbed.linux.mak all
 ERRORLEVEL=$?
 if [ $ERRORLEVEL -ne 0 ]
 then
-echo "Error:"$ERRORLEVEL && exit
-fi
-
-echo "All assemblies built successfully."
